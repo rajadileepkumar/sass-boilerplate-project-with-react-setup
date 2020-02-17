52cc3452f52ea672f699ecbad60c8223fa6f9ddb
rajadileep.kumar <rajadileep.kumar@t-3.com>
2020-02-17 17:11:30 +0530

    The basic sass configuration with some default mixins added|WIP

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# sass-boilerplate-project-with-react-setup
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './styles/index.scss'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="m-all-100 p-top-100">Hello World</h1>
+      </div>
+    )
+  }
+}
